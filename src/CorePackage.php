5cc60eca682c36c032e7c1cf1e5d2a273a23b52a
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-25 16:10:32 +0200

    Rewrite config to use Yaml format.

--- src/CorePackage.php
+++ src/CorePackage.php
@@ -6,5 +6,4 @@ use ROrier\Core\Foundations\AbstractPackage;
 
 class CorePackage extends AbstractPackage
 {
-    protected const CONFIG_LOADER = 'ROrier\Core\Components\ConfigLoaders\JsonLoader';
 }
