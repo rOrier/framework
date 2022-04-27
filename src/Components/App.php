0037635496a7e87623da12d07158d69a95acd5de
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-27 16:53:18 +0200

    Create abstract App component.

--- src/Components/App.php
+++ src/Components/App.php
@@ -2,22 +2,13 @@
 
 namespace ROrier\Core\Components;
 
-use Exception;
 use ROrier\Config\Interfaces\ParametersInterface;
 use ROrier\Container\Interfaces\ContainerInterface;
-use ROrier\Core\Interfaces\AppInterface;
+use ROrier\Core\Foundations\AbstractApp;
 use ROrier\Core\Interfaces\KernelInterface;
 
-class App implements AppInterface
+class App extends AbstractApp
 {
-    private string $root;
-
-    private KernelInterface $kernel;
-
-    private ParametersInterface $parameters;
-
-    private ContainerInterface $container;
-
     public function __construct(string $root)
     {
         $this->root = $root;
@@ -51,49 +42,5 @@ class App implements AppInterface
         $this->container = $container;
 
         return $this;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getRoot(): string
-    {
-        return $this->root;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getKernel(): KernelInterface
-    {
-        if (!isset($this->kernel)) {
-            throw new Exception("No kernel defined.");
-        }
-
-        return $this->kernel;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getParameter(string $address)
-    {
-        if (!isset($this->parameters)) {
-            throw new Exception("No parameters defined.");
-        }
-
-        return $this->parameters[$address];
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getService(string $id): object
-    {
-        if (!isset($this->container)) {
-            throw new Exception("No container defined.");
-        }
-
-        return $this->container->get($id);
     }
 }
