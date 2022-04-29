ef7840c7534d0525ca0c0ea46a428ed78e28a5b4
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-29 06:37:32 +0200

    Add capability to store App object directly in Main class.

--- src/Main.php
+++ src/Main.php
@@ -2,11 +2,15 @@
 
 namespace ROrier\Core;
 
+use Exception;
 use ROrier\Core\Components\Boot;
 use ROrier\Core\Components\Bootstrapper;
+use ROrier\Core\Interfaces\AppInterface;
 
 abstract class Main
 {
+    static private ?AppInterface $app = null;
+
     static public function boot(): Bootstrapper
     {
         $boot = new Boot();
@@ -28,5 +32,23 @@ abstract class Main
     static protected function isValidTrace($trace)
     {
         return is_array($trace) && array_key_exists('file', $trace) && !empty($trace['file']);
+    }
+
+    /**
+     * @return AppInterface
+     * @throws Exception
+     */
+    public static function app(): AppInterface
+    {
+        if (static::$app === null) {
+            throw new Exception("App is undefined. Use boot() method to build application.");
+        }
+
+        return self::$app;
+    }
+
+    static public function save(AppInterface $app)
+    {
+        static::$app = $app;
     }
 }
